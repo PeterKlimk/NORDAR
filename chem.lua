b2256db96becd61b3da189de7588b298d8fb4561
pkzmbk <peterklimk@outlook.com>
2022-09-30 19:13:08 +1000

    test

---
+++ chem.lua
@@ -0,0 +1,12 @@
+local component = require("component")
+
+transposer_fluids = 
+transposer_machine = 
+adapter_machine = 
+
+function check_stock()
+    fluids = transposer_fluids.getFluidInTank(1)
+    for k, v in pairs()
+return
+
+check_stock()
